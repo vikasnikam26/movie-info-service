9153cd5f3afc42a25093956aebe120bf1580b0be
Vikas Nikam <vikas.nikam@exfo.com>
2021-06-26 22:55:31 +0530

    Initial commit for Movie Info service

---
+++ src/test/java/com/viknik1/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viknik1.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
